9f31ce855b263239bfccfba24d831fc4d1034a17
vallabhs1216 <100321435+vallabhs1216@users.noreply.github.com>
2022-04-05 14:06:21 +1200

    Add files via upload

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -1,4 +1,58 @@
-#
+# Funtions go here
+
+
+# Puts a series of symbols at the start and end of text
+def statement_generator(text, decoration):
+
+    # Makes a 5 character string
+    ends = decoration * 5
+
+    # adds decoration to the statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print
+
+    return ""
+
+# Displays instructions
+def instructions():
+
+    statement_generator("Instructions / Information", "=")
+    print("Please choose a facotr between 1 and 200 ")
+    print()
+    print("This programs assumes the factors are whole numbers. ")
+    print()
+    print("Press <enter> at the end of each calculation or any key to quit. ")
+    print()
+    return 
+
+    
+# checks input is a number more than a givevn value
+def num_check(question, low, high):
+    valid = False
+    while not valid:
+        
+        error_1= "Please enter a factor that is more than (or equal to) 1 and less (or equal to) 200"
+        error_2= "Please enter a factor that does not have decimal part / enter a number"
+                                                                         
+        try:
+        
+            # Ask user to enter a number
+            response = int(input(question))
+
+            # Checks number is more than zero
+            if response >= low and response <= high:
+                return response
+
+            # Outputs error if input is invalid
+            else:
+                print(error_1)
+                print()
+                
+        except ValueError:
+            print(error_2)
 
 
 # Main routine goes here
@@ -20,7 +74,7 @@ while keep_going == "":
     comment = ""
 
     # ask a user fo number to be factored
-    var_to_factor = num_check("Number? ")
+    var_to_factor = num_check("Enter an integer: ")
 
     if var_to_factor != 1:
         factor_list = get_factors(var_to_factor)
